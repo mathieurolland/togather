d755fe729d1b87b4fdf57eec1dbf1bc4efe03d16
Mathieu Rolland <mathieu.rolland@edu.escpeurope.eu>
2016-08-23 11:16:17 +0200

    validations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,4 +2,5 @@ class Meeting < ApplicationRecord
   belongs_to :connection
   belongs_to :place
   has_many :messages
+  validates :date, presence: true
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,11 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :meetings
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :address, uniqueness: true
+  validates :description, presence: true
+  validates :phone_number, presence: true
+  validates :phone_number, uniqueness: true
+  validates :type, presence: true
 end
