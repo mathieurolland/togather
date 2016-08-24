2e8f8cf0f01c4f1b9e5f58c6c77783178c2f19a2
Mathieu Rolland <mathieu.rolland@edu.escpeurope.eu>
2016-08-24 12:32:58 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'recommended_users/index'
-
-  get 'recommended_users/create'
-
-  get 'recommended_users/update'
-
-  get 'recommended_users/destroy'
-
   devise_for :users
   root to: 'pages#home'
   root to: "pages#dashboard", constraints: lambda { |r| r.env["warden"].authenticate? }
   get "/dashboard/", to: "pages#dashboard"
+  resource :recommended_users, only: [:destroy, :create, :update]
   resources :categories, only: [:index]
   resources :skills, only: [:index]
   resources :user_skills, only: [:create, :destroy]
