525d115655b0cb31881a833ee9e625e32c6156a6
Mathieu Rolland <mathieu.rolland@edu.escpeurope.eu>
2016-08-24 12:29:27 +0200

    route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   root to: "pages#dashboard", constraints: lambda { |r| r.env["warden"].authenticate? }
   get "/dashboard/", to: "pages#dashboard"
+  resources :recommended_users, only: [:create, :update, :destroy]
   resources :categories, only: [:index]
   resources :skills, only: [:index]
   resources :user_skills, only: [:create, :destroy]
