b9dad95762f2aaa3e0a7618f21778ed9e0960191
Loan Robert <loan.rbrt@gmail.com>
2016-08-27 15:19:00 +0200

    some changes and autofocus

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,11 @@
 //= require gmaps/google
 //= require turbolinks
 //= require sync
+
+$('#signUpModal').on('shown.bs.modal', function () {
+  $('#user_first_name').focus();
+});
+
+$('#loginModal').on('shown.bs.modal', function () {
+  $('#user_email').focus();
+});
