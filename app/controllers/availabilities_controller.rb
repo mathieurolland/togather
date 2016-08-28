3cf40bb035b0838d5a3ab11cea1897c6d0420e5b
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-08-28 17:23:53 +0200

    crud availabilities partner

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -1,25 +1,13 @@
 class AvailabilitiesController < ApplicationController
+  before_action :defind_arrays, only: [ :index, :create, :edit, :update ]
+  before_action :find_availability, only: [ :edit, :update, :destroy ]
+  before_action :save_availability, only: [ :create, :update ]
 
   def index
-    @place = Place.find(params[:place_id])
-    @days = ["Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    @hours = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
-    @minutes = ["00", "10", "20", "30", "40", "50"]
   end
 
   def create
-    @place = Place.find(params[:place_id])
-    @days = ["Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    @hours = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
-    @minutes = ["00", "10", "20", "30", "40", "50"]
-    p = new_date_params
-    max_time = Time.utc(2000, 1, 1, p[:max_hour].to_i, p[:max_minute].to_i )
-    min_time = Time.utc(2000, 1, 1, p[:min_hour].to_i, p[:min_minute].to_i )
-    @availability = @place.availabilities.new(  max_day: p[:max_day],
-                                                min_day: p[:min_day],
-                                                max_time: max_time,
-                                                min_time: min_time)
-    if @days.index(p[:max_day]) > @days.index(p[:min_day]) && max_time > min_time && @availability.save
+    if valid_form?
       redirect_to place_availabilities_path(@place)
     else
       render :index
@@ -27,23 +15,10 @@ class AvailabilitiesController < ApplicationController
   end
 
   def edit
-    @availability = Availability.find(params[:id])
-    @place = Place.find(params[:place_id])
   end
 
   def update
-    @availability = Availability.find(params[:id])
-    @days = ["Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    @hours = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
-    @minutes = ["00", "10", "20", "30", "40", "50"]
-    p = new_date_params
-    max_time = Time.utc(2000, 1, 1, p[:max_hour].to_i, p[:max_minute].to_i )
-    min_time = Time.utc(2000, 1, 1, p[:min_hour].to_i, p[:min_minute].to_i )
-    @availability = @place.availabilities.new(  max_day: p[:max_day],
-                                                min_day: p[:min_day],
-                                                max_time: max_time,
-                                                min_time: min_time)
-    if @days.index(p[:max_day]) > @days.index(p[:min_day]) && max_time > min_time && @availability.save
+    if valid_form?
       redirect_to place_availabilities_path(@place)
     else
       render :edit
@@ -51,7 +26,6 @@ class AvailabilitiesController < ApplicationController
   end
 
   def destroy
-    @availability = Availability.find(params[:id])
     @availability.destroy
     redirect_to place_availabilities_path(@availability.place, @availability )
   end
@@ -62,6 +36,30 @@ class AvailabilitiesController < ApplicationController
     params.require(:new_date).permit!
   end
 
+  def defind_arrays
+    @place = Place.find(params[:place_id])
+    @days = ["Monday", "Tuesday", "Wenesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    @hours = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
+    @minutes = ["00", "10", "20", "30", "40", "50"]
+  end
+
+  def find_availability
+    @availability = Availability.find(params[:id])
+  end
+
+  def save_availability
+    @p = new_date_params
+    @max_time = Time.utc(2000, 1, 1, @p[:max_hour].to_i, @p[:max_minute].to_i )
+    @min_time = Time.utc(2000, 1, 1, @p[:min_hour].to_i, @p[:min_minute].to_i )
+    @availability = @place.availabilities.new(  max_day: @p[:max_day],
+                                                min_day: @p[:min_day],
+                                                max_time: @max_time,
+                                                min_time: @min_time)
+  end
+
+  def valid_form?
+    @days.index(@p[:max_day]) > @days.index(@p[:min_day]) && @max_time > @min_time && @availability.save
+  end
 end
 
 
