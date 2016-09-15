8dc4bf4cb70cce76821fa39d08f403de4b5c5266
Jonathan Serafini <jonathan.serafini@gmail.com>
2016-09-15 12:06:36 +0200

    erreur de frappe

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -36,7 +36,7 @@ class PagesController < ApplicationController
       @hashsuggestions = hash_geocoder_suggestions(suggestions)
       @hashplaces = hash_geocoder_places(@places)
     else
-      current_user.update(photo_url: "https://t3.ftcdn.net/jpg/00/53/51/66/160_F_53516657_zFxg5J7wKRaRmjn00ez578LuO8WuSOOH.jpg")
+      current_user.update(photo_url: "https://scontent.xx.fbcdn.net/v/t1.0-9/13393913_10153647120416299_6568716079801929041_n.jpg?oh=d469bbf9cd538a46cce11a8673a42bea&oe=5849A552")
       godfather = RecommendedUser.find_by_email(current_user.email).user
       Connection.create!(status: "connected", guest: godfather, host: current_user)
       redirect_to categories_path
